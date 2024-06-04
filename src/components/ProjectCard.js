b26fbdc2fa86d6480fe56fc82e39fbd83d57176a
Hernan Damazo <raulclassico7@gmail.com>
2024-06-04 01:05:09 -0700

    portfolio updates

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,14 @@ import { ArrowRightCircle } from 'react-bootstrap-icons'
 
 
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, project }) => {
+
+    let projectUrl = '';
+        if(project === "Reactype") {
+            projectUrl = "https://www.reactype.dev/";
+        } else if(project === "SJ") {
+            projectUrl = "https://www.sanjuanautomotive.com/";
+        }
 
     return (
         <Col sm={4} md={6}>
@@ -14,7 +21,7 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
                 <h4>{title}</h4>
                 <span>{description}</span>
                     <div>
-                        <a href="https://www.reactype.dev/" target="_blank" rel='noopener noreferrer'><button className="project-link" onClick={() => console.log('connect')}>View Project<ArrowRightCircle size={20} /></button></a>
+                        <a href={projectUrl} target="_blank" rel='noopener noreferrer'><button className="project-link" onClick={() => console.log('connect')}>View Project<ArrowRightCircle size={20} /></button></a>
                     </div>
                 </div>
             </div>
