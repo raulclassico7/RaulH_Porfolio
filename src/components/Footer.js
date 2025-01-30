43838802a81806659c9f299f7a1c3b31e598ba66
Raul Damazo <raulclassico7@gmail.com>
2025-01-29 17:05:26 -0800

    Mobile friendly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,27 +2,30 @@ import { Col, Container, Row } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import footerLogo from '../assets/img/Damazo Logo-PhotoRoom.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/github-icon5-PhotoRoom.png'
+import navIcon2 from '../assets/img/github-icon5-PhotoRoom.png';
 
 export const Footer = () => {
-
-    return (
-        <footer className="footer">
-            <Container>
-                <Row className='align-item-center'>
-                    <MailchimpForm/>
-                    <Col size={12} sm={6}>
-                        <img src={footerLogo} alt='logo'/>
-                    </Col>
-                    <Col size={12} sm={6} className="text-center text-sm-end">
-                        <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/raul-hernan-damazo-chang/"><img src={navIcon1} alt="linkedIn Icon"/></a>
-                            <a href="https://github.com/raulclassico7"><img className="git-icon" src={navIcon2} alt="Github Icon"/></a>
-                        </div>
-                        <p>CopyRight 2022. All Right Reserved</p>
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className='align-items-center'>
+          <MailchimpForm />
+          <Col xs={12} sm={6} className="text-center text-sm-start">
+            <img src={footerLogo} alt='logo' />
+          </Col>
+          <Col xs={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/raul-hernan-damazo-chang/">
+                <img src={navIcon1} alt="LinkedIn Icon" />
+              </a>
+              <a href="https://github.com/raulclassico7">
+                <img className="git-icon" src={navIcon2} alt="Github Icon" />
+              </a>
+            </div>
+            <p>CopyRight 2022. All Right Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,67 +1,70 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
-import logo from '../assets/img/Damazo Logo-PhotoRoom.png'
-import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/github-icon5-PhotoRoom.png'
+import logo from '../assets/img/Damazo Logo-PhotoRoom.png';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/github-icon5-PhotoRoom.png';
 import { HashLink } from 'react-router-hash-link';
-import {
-    BrowserRouter as Router
-} from 'react-router-dom';
-
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+  const [showToggle, setShowToggle] = useState(false); // Toggle button visibility
 
-    useEffect(() => {
-        const onScroll = () => {
-            if(window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        }
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 80) {
+        setScrolled(true);
+        setShowToggle(true); // Show toggle button when scrolled down
+      } else {
+        setScrolled(false);
+        setShowToggle(false); // Hide toggle button when at the top
+      }
+    };
 
-        window.addEventListener("scroll", onScroll);
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [])
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-    const onUpdateLink = (linkStr) => {
-        setActiveLink(linkStr);
-    }
+  const onUpdateLink = (linkStr) => {
+    setActiveLink(linkStr);
+  };
 
+  return (
+    <Router>
+      <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} className="logo-nav" alt="Logo" />
+          </Navbar.Brand>
+          
+          {/* Conditionally render Navbar.Toggle */}
+          {showToggle && (
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-navbar-toggle">
+              <span className="navbar-toggler-icon"></span>
+            </Navbar.Toggle>
+          )}
 
-    return (
-        <Router>
-        <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
-            <Container>
-                <Navbar.Brand href="/">
-                    <img src={logo} className="logo-nav" alt="Logo"/>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className="navbar-toogle-icon"></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateLink('home')}>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateLink('skills')}>Skills</Nav.Link>
-                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateLink('projects')}>Projects</Nav.Link>
-                    </Nav>
-                    <span className="navbar-text">
-                        <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/raul-hernan-damazo-chang/" target="_blank" rel='noopener noreferrer'><img src={navIcon1} alt="" /></a>
-                            <a href="https://github.com/raulclassico7" target="_blank" rel='noopener noreferrer'><img className="git-icon" src={navIcon2} alt="" /></a>
-                        </div>
-                        <HashLink to='#connect'>
-                        <button className="vvd" onClick={() => console.log('connect')}>
-                            <span>Let's Connect!</span>
-                        </button>
-                        </HashLink>
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        </Router>
-    )
-}
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateLink('projects')}>Projects</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="https://www.linkedin.com/in/raul-hernan-damazo-chang/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn Icon" /></a>
+                <a href="https://github.com/raulclassico7" target="_blank" rel="noopener noreferrer"><img className="git-icon" src={navIcon2} alt="Github Icon" /></a>
+              </div>
+              <HashLink to='#connect'>
+                <button className="vvd" onClick={() => console.log('connect')}>
+                  <span>Let's Connect!</span>
+                </button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  );
+};
